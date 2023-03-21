12d2a3ac2d7ce4362f29be765bb85674924bc2e7
Daniel Pires <dfpires@gmail.com>
2023-03-21 20:48:03 -0300

    Add files via upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -24,6 +24,7 @@ export async function AppRoutes(app: FastifyInstance){
           )
         })
         const { title, weekDays } = createHabitBody.parse(request.body)
+        // cria a data com a hora 20230321:00:00
         const today = dayjs().startOf('day').toDate()
         await prisma.habit.create({
           data: {
@@ -39,5 +40,47 @@ export async function AppRoutes(app: FastifyInstance){
           }
         })
       })
-    
+      
+      app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date() // converte string para date
+        })
+        const {date} = getDayParams.parse(request.query)
+        // data do usuário com 20230102:00:00:00
+        const parsedDate = dayjs(date).startOf('day')
+          //segunda é 1
+        const weekDay = parsedDate.get('day')
+        // todos hábitos possívels
+        // hábito precisa estar disponível no dia da semana
+        // e não pode ter sido criado depois da data
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at:{
+                    lte: date
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+        // todos hábitos realizados
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+        const completedHabit = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+        return {
+            possibleHabits,
+            completedHabit
+        }
+    })
+
 }
